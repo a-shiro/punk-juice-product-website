5af1129be9b6d63965a1f4801d299ec0ddb767fc
a-shiro <alekmitov20@gmail.com>
2023-07-27 09:58:42 +0300

    Add: Component files

---
+++ src/pages/home/components/Hero/Hero.js
@@ -0,0 +1,27 @@
+import heroVideo from "../../../../videos/PunkJuice.mov";
+import styles from "./Hero.module.css";
+
+function Hero() {
+  return (
+    <section className={styles.heroContainer}>
+      <div className={styles.heroText}>
+        <h1>Punk Juice</h1>
+
+        <p>
+          for those who <strong>rock</strong>
+        </p>
+      </div>
+
+      <div className={styles.videoWrapper}>
+        <video autoPlay loop muted>
+          <source src={heroVideo} type="video/mp4" />
+          Your browser does not support .mov video files.
+        </video>
+      </div>
+
+      <div className={styles.heroOverlay}></div>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/pages/home/components/Nav/Nav.js
@@ -0,0 +1,37 @@
+import styles from "./Nav.module.css";
+
+function Nav() {
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.container}>
+        <a>Punk Juice</a>
+
+        <button className={styles.hamburger}>X</button>
+
+        <div className={styles.listWrapper}>
+          <ul class={styles.navbarList}>
+            <li>
+              <a className={`${styles.link} ${styles.active}`} href="#hero">
+                Home
+              </a>
+            </li>
+
+            <li>
+              <a className={`${styles.link}`} href="#portfolio">
+                Product
+              </a>
+            </li>
+
+            <li>
+              <a className={`${styles.link}`} href="#news">
+                News & Events
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
