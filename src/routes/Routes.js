6daac44c4ffd490a433222c674ea29349ede9ef0
a-shiro <alekmitov20@gmail.com>
2023-08-02 11:48:23 +0300

    ---

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/home/Home";
-import Details from "../pages/home/article-details/Details";
+import Details from "../pages/article-details/Details";
 
 function RoutesManager() {
   return (
