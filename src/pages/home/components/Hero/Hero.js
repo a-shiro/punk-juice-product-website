2092b00c93f28e41b2ac86502dfed49db9ebed6c
a-shiro <alekmitov20@gmail.com>
2023-08-24 11:53:00 +0300

    Refactored code, Optimized video

--- src/pages/home/components/Hero/Hero.js
+++ src/pages/home/components/Hero/Hero.js
@@ -1,25 +1,24 @@
-import heroVideo from "../../../../assets/videos/PunkJuice.mov";
 import styles from "./Hero.module.css";
+import videoMp4 from "../../../../assets/videos/PunkJuice.mp4";
+import videoWebm from "../../../../assets/videos/PunkJuice.webm";
 
 function Hero() {
   return (
-    <section id="hero" className={styles.heroContainer}>
-      <div className={styles.heroText}>
+    <section id="hero" className={styles.section}>
+      <header className={styles.header}>
         <h1>Punk Juice</h1>
-
         <p>
           for those who <strong>rock</strong>
         </p>
-      </div>
+      </header>
 
-      <div className={styles.videoWrapper}>
-        <video autoPlay loop muted>
-          <source src={heroVideo} type="video/mp4" />
-          Your browser does not support .mov video files.
-        </video>
-      </div>
+      <video className={styles.backgroundVideo} playsInline autoPlay muted loop>
+        <source src={videoWebm} type="video/webm" />
+        <source src={videoMp4} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-      <div className={styles.heroOverlay}></div>
+      <div className={styles.overlay}></div>
     </section>
   );
 }
