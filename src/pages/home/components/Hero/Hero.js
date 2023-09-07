ead751829b18efeb3a38ece88bff5396896b82f9
a-shiro <alekmitov20@gmail.com>
2023-09-07 14:47:08 +0300

    Cleaned code

--- src/pages/home/components/Hero/Hero.js
+++ src/pages/home/components/Hero/Hero.js
@@ -1,19 +1,19 @@
+// CSS
 import styles from "./Hero.module.css";
+// Static files
 import videoMp4 from "../../../../assets/videos/PunkJuice.mp4";
-import videoWebm from "../../../../assets/videos/PunkJuice.webm";
 
 function Hero() {
   return (
-    <section id="hero" className={styles.header}>
-      <div className={styles.container}>
+    <section id="hero" className={styles.section}>
+      <hgroup className={styles.headerTitles}>
         <h1>Punk Juice</h1>
-        <p>
+        <h2>
           for those who <strong>rock</strong>
-        </p>
-      </div>
+        </h2>
+      </hgroup>
 
       <video className={styles.backgroundVideo} playsInline autoPlay muted loop>
-        <source src={videoWebm} type="video/webm" />
         <source src={videoMp4} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
