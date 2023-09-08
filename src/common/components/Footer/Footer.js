2220417bbe54be32e40e4872a3c45d1aa6826b6e
a-shiro <alekmitov20@gmail.com>
2023-09-08 11:12:16 +0300

    Cleaned code

--- src/common/components/Footer/Footer.js
+++ src/common/components/Footer/Footer.js
@@ -1,4 +1,6 @@
+// CSS
 import styles from "./Footer.module.css";
+// Static files
 import locationIcon from "../../../assets/icons/location.png";
 import youtubeIcon from "../../../assets/icons/youtube.png";
 import facebookIcon from "../../../assets/icons/facebook.png";
@@ -9,34 +11,32 @@ import instagramIcon from "../../../assets/icons/instagram.png";
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.container}>
-        <address className={styles.title}>
-          <img alt="pin" className={styles.icon} src={locationIcon} />
-          State of California, USA
-        </address>
+      <address>
+        <img alt="location-pin" className={styles.icon} src={locationIcon} />
+        <span className={styles.address}>State of California, USA</span>
+      </address>
 
-        <span className={styles.email}>punkjuice@mail.com</span>
+      <span className={styles.email}>punkjuice@mail.com</span>
 
-        <ul className={styles.socials}>
-          <li>
-            <img alt="instagram" src={instagramIcon} className={styles.icon} />
-          </li>
-          <li>
-            <img alt="facebook" src={facebookIcon} className={styles.icon} />
-          </li>
-          <li>
-            <img alt="twitter" src={twitterIcon} className={styles.icon} />
-          </li>
-          <li>
-            <img alt="whatsapp" src={whatsappIcon} className={styles.icon} />
-          </li>
-          <li>
-            <img alt="youtube" src={youtubeIcon} className={styles.icon} />
-          </li>
-        </ul>
+      <ul className={styles.socials}>
+        <li>
+          <img alt="instagram" src={instagramIcon} className={styles.icon} />
+        </li>
+        <li>
+          <img alt="facebook" src={facebookIcon} className={styles.icon} />
+        </li>
+        <li>
+          <img alt="twitter" src={twitterIcon} className={styles.icon} />
+        </li>
+        <li>
+          <img alt="whatsapp" src={whatsappIcon} className={styles.icon} />
+        </li>
+        <li>
+          <img alt="youtube" src={youtubeIcon} className={styles.icon} />
+        </li>
+      </ul>
 
-        <p className={styles.copyright}>Copyright © Punk Juice 2023 </p>
-      </div>
+      <p className={styles.copyright}>Copyright © Punk Juice 2023 </p>
     </footer>
   );
 }
