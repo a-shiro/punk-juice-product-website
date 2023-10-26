ee8ad2f74e44806e3ccce4b46396d1920c4b90fe
a-shiro <alekmitov20@gmail.com>
2023-10-26 09:46:37 +0300

    Add: Conditional data fetch function

--- src/services/queries.js
+++ src/services/queries.js
@@ -1,5 +1,5 @@
 import { database } from "../config/firebase/sdk";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query } from "firebase/firestore";
 
 export async function fetchData(collectionName) {
   try {
@@ -15,3 +15,23 @@ export async function fetchData(collectionName) {
     console.error(err);
   }
 }
+
+export async function fetchDataWhere(collectionName, queryConditions = []) {
+  try {
+    const collectionRef = collection(database, collectionName);
+
+    const q = query(collectionRef, ...queryConditions);
+    const data = await getDocs(q);
+
+    const filteredData = data.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+
+    return filteredData;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// TODO: Add proper error handle to queries
